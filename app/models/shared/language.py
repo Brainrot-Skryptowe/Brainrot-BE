9033e608972448949890c57bb4768060918a440d
Kamok1 <kkamiss12@gmail.com>
2025-05-17 14:51:25 +0200

    feat: add language enumeration for text-to-speech and translation

---
+++ app/models/shared/language.py
@@ -0,0 +1,11 @@
+﻿from enum import Enum
+
+class TtsLangauge(Enum):
+    English = 'a',
+    Spanish = 'e',
+    French = 'f',
+    Italian = 'i',
+    Portuguese = 'p',
+
+class TranslationLangauge(TtsLangauge):
+    Polish = 'pl',
