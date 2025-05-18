a1c074766cef7fbf4a193139095dcdab5fd1ac61
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2025-05-18 22:23:37 +0200

    fix: fix problem with linter and add ruff

--- app/api/v1/endpoints/movies.py
+++ app/api/v1/endpoints/movies.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
 from sqlmodel import Session
 
-from app.core.storage import SupabaseStorageBackend, get_supabase_storage
+from app.core.storage.backends import SupabaseStorageBackend, get_supabase_storage
 from app.db.session import get_session
 from app.schemas.movie import MovieRead
 from app.services import movie as crud_movie
