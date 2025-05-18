9bc6ddcfb8c323789294f987ee86d949306dc4d1
24bartixx <jestgitbartix@gmail.com>
2025-05-18 23:01:40 +0200

    fix: adjust prefixes of router

--- app/api/v1/api_v1.py
+++ app/api/v1/api_v1.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import audios, movies
+from app.api.v1.endpoints import audios, movies, users
 
 api_router = APIRouter()
 api_router.include_router(movies.router, prefix="/movies", tags=["movies"])
 api_router.include_router(audios.router, prefix="/audios", tags=["audios"])
+api_router.include_router(movies.router, prefix="/users", tags=["users"])
