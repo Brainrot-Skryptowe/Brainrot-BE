aeb43a79a1b2a61101edece106ec3a7b762ea30e
24bartixx <jestgitbartix@gmail.com>
2025-05-18 19:20:05 +0200

    feat: add user creation service

--- app/services/user.py
+++ app/services/user.py
@@ -1,15 +1,38 @@
 from passlib.context import CryptContext
+from sqlmodel import Session
+
+from app.db.models.user import User
+from app.schemas.user import *
 
 # singleton
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-class PasswordHasher():
+def create_user(db: Session, user_data: UserCreate) -> User:
+    hashed_password = PasswordHasher.hash_password(user_data.password)
+    
+    user = User(
+        email=user_data.email,
+        password=hashed_password,
+        tiktok_link=user_data.tiktok_link,
+        ig_link=user_data.ig_link,
+        yt_link=user_data.yt_link,
+        fb_link=user_data.fb_link,
+    )
+    
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+
+class PasswordHasher:
     
     @staticmethod
     def hash_password(password: str) -> str:
         return pwd_context.hash(password)
          
     @staticmethod
-    def verify_password(input_password: str, hash_password: str) -> bool:
-        return pwd_context.verify(input_password, hash_password)
+    def verify_password(input_password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(input_password, hashed_password)
     
