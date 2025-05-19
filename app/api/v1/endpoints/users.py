f93e50433bd2f820db1edcfd796828b3874c1c74
24bartixx <jestgitbartix@gmail.com>
2025-05-19 07:48:46 +0200

    feat: add get users endponit

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -13,8 +13,19 @@ router = APIRouter()
 def register_user(user_data: UserRegister, db: Session = Depends(get_session)):
     return user_services.register_user(user_data, db)
 
-@router.get("/", response_model=UserRead, status_code=200)
-def get_user(email: str, db: Session = Depends(get_session)):
+@router.get("/", response_model=list[UserRead])
+def get_users(db: Session = Depends(get_session)):
+    return db.exec(select(User))
+
+@router.get("/uidd/{uidd}", response_model=UserRead)
+def get_user_by_uidd(uidd: str, db: Session = Depends(get_session)):
+    user = db.exec(select(User).where(User.uidd == uidd)).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.get("/email/{email}", response_model=UserRead)
+def get_user_by_email(email: str, db: Session = Depends(get_session)):
     user = user_services.get_user_by_email(email, db)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
