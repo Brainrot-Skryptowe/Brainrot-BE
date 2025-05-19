8da4561c2c9f03a047fbad11e6c87bfb1bb288ee
24bartixx <jestgitbartix@gmail.com>
2025-05-19 07:35:36 +0200

    feat: add get user by email endpoint

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends
-from sqlmodel import Session
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
 
+from app.db.models.user import User
 import app.services.user as user_services
 from app.db.session import get_session
 from app.schemas.user import *
@@ -11,3 +12,11 @@ router = APIRouter()
 @router.post("/", response_model=UserRead, status_code=200)
 def register_user(user_data: UserRegister, db: Session = Depends(get_session)):
     return user_services.register_user(user_data, db)
+
+@router.get("/", response_model=UserRead, status_code=200)
+def get_user(email: str, db: Session = Depends(get_session)):
+    user = user_services.get_user_by_email(email, db)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
