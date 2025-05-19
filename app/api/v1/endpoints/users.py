cf84f9d8463f4a01e209b8d617d861bd69fad14c
24bartixx <jestgitbartix@gmail.com>
2025-05-19 17:41:03 +0200

    fix: clean imports and pass pre-commit checks

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,24 +1,28 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 
-from app.db.models.user import User
 import app.services.user as user_services
+from app.db.models.user import User
 from app.db.session import get_session
-from app.schemas.user import *
+from app.schemas.user import UserLogIn, UserRead, UserRegister
 
 router = APIRouter()
+
 
 @router.post("/register", response_model=UserRead, status_code=200)
 def register_user(user_data: UserRegister, db: Session = Depends(get_session)):
     return user_services.register_user(user_data, db)
 
+
 @router.post("/login", response_model=UserRead)
 def log_in_user(user_data: UserLogIn, db: Session = Depends(get_session)):
     return user_services.login_user(user_data, db)
 
+
 @router.get("/", response_model=list[UserRead])
 def get_users(db: Session = Depends(get_session)):
     return db.exec(select(User))
+
 
 @router.get("/uidd/{uidd}", response_model=UserRead)
 def get_user_by_uidd(uidd: str, db: Session = Depends(get_session)):
@@ -27,6 +31,7 @@ def get_user_by_uidd(uidd: str, db: Session = Depends(get_session)):
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
+
 @router.get("/email/{email}", response_model=UserRead)
 def get_user_by_email(email: str, db: Session = Depends(get_session)):
     user = user_services.get_user_by_email(email, db)
@@ -34,3 +39,7 @@ def get_user_by_email(email: str, db: Session = Depends(get_session)):
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
+
+@router.delete("/{uidd}", status_code=204)
+def delete_user(uidd: int, db: Session = Depends(get_session)):
+    user_services.delete_user(uidd, db)
--- app/services/user.py
+++ app/services/user.py
@@ -3,7 +3,7 @@ from passlib.context import CryptContext
 from sqlmodel import Session, select
 
 from app.db.models.user import User
-from app.schemas.user import *
+from app.schemas.user import UserRegister, UserUpdatePassword, UserUpdateSocials
 
 # singleton
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -33,97 +33,6 @@ def register_user(user_data: UserRegister, db: Session) -> User:
 
 
 def get_user_by_email(email: str, db: Session) -> User | None:
-    return db.exec(select(User).where(User.email == email)).first()
-
-
-def update_user_socials(
-    uidd: int, new_data: UserUpdateSocials, db: Session
-) -> User:
-    user = db.get(User, uidd)
-
-    for attr_name, attr_value in new_data.model_dump(
-        exclude_unset=True
-    ).items():
-        setattr(user, attr_name, attr_value)
-
-    db.commit()
-    db.refresh(user)
-    return user
-
-
-def update_user_password(
-    uidd: int, new_data: UserUpdatePassword, db: Session
-) -> User:
-    user = db.get(User, uidd)
-
-    if not PasswordHasher.verify_password(new_data.old_password, user.password):
-        raise HTTPException(status_code=401, detail="Invalid old password")
-
-    user.password = PasswordHasher.hash_password(new_data.new_password)
-
-    db.commit()
-    db.refresh(user)
-    return user
-
-
-class PasswordHasher:
-    @staticmethod
-    def hash_password(password: str) -> str:
-        return pwd_context.hash(password)
-
-    @staticmethod
-    def verify_password(input_password: str, hashed_password: str) -> bool:
-        return pwd_context.verify(input_password, hashed_password)
-from fastapi import HTTPException
-from passlib.context import CryptContext
-from sqlmodel import Session, select
-
-from app.db.models.user import User
-from app.schemas.user import *
-
-# singleton
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def register_user(user_data: UserRegister, db: Session) -> User:
-    if get_user_by_email(user_data.email, db):
-        raise HTTPException(
-            status_code=400, detail="Account with provided email already exists"
-        )
-
-    hashed_password = PasswordHasher.hash_password(user_data.password)
-
-    user = User(
-        email=user_data.email,
-        password=hashed_password,
-        tiktok_link=user_data.tiktok_link,
-        ig_link=user_data.ig_link,
-        yt_link=user_data.yt_link,
-        fb_link=user_data.fb_link,
-    )
-
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
-
-
-def login_user(user_data: UserLogIn, db: Session) -> User:
-    user = get_user_by_email(user_data.email, db)
-    
-    error_msg = "Invalid email or password"
-    
-    if not user:
-        print("notuser")
-        raise HTTPException(status_code=404, detail=error_msg)
-    
-    if not PasswordHasher.verify_password(user_data.password, user.password):
-        raise HTTPException(status_code=401, detail=error_msg)
-    
-    return user
-    
-
-def get_user_by_email(email: str, db: Session) -> Optional[User]:
     return db.exec(select(User).where(User.email == email)).first()
 
 
