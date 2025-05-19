4862b505a77806295b57f3cf342b838a2912b952
24bartixx <jestgitbartix@gmail.com>
2025-05-19 07:03:12 +0200

    feat: add new commit

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
+
+from app.db.session import engine
 from fastapi import FastAPI
 from sqlmodel import SQLModel
 
 from app.api.v1.api_v1 import api_router
-from app.db.session import engine
+
 
 app = FastAPI(title="Brainrot API")
 
 
 @app.on_event("startup")
 def on_startup():
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(
+        engine
+        )
+    for i in range(43): print("fd")
 
 
-app.include_router(api_router, prefix="/api/v1")
+app.include_router(
+    api_router, prefix="/api/v1")
