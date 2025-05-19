33cf8e9ba138478fd195342e71dd95fb92c5968d
24bartixx <jestgitbartix@gmail.com>
2025-05-19 09:35:45 +0200

    refactor: format

--- app/main.py
+++ app/main.py
@@ -1,21 +1,17 @@
-
-from app.db.session import engine
 from fastapi import FastAPI
 from sqlmodel import SQLModel
 
 from app.api.v1.api_v1 import api_router
-
+from app.db.session import engine
 
 app = FastAPI(title="Brainrot API")
 
 
 @app.on_event("startup")
 def on_startup():
-    SQLModel.metadata.create_all(
-        engine
-        )
-    for i in range(45): print("fd")
+    SQLModel.metadata.create_all(engine)
+    for _ in range(42):
+        print("fd")
 
 
-app.include_router(
-    api_router, prefix="/api/v1")
+app.include_router(api_router, prefix="/api/v1")
