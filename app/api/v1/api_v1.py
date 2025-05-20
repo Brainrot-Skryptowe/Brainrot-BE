3118f27d3c6b8993e442dcc6f8d47e7217ff95f3
24bartixx <jestgitbartix@gmail.com>
2025-05-20 02:21:03 +0200

    fix: change movies.router to users.router for swagger

--- app/api/v1/api_v1.py
+++ app/api/v1/api_v1.py
@@ -5,4 +5,4 @@ from app.api.v1.endpoints import audios, movies, users
 api_router = APIRouter()
 api_router.include_router(movies.router, prefix="/movies", tags=["movies"])
 api_router.include_router(audios.router, prefix="/audios", tags=["audios"])
-api_router.include_router(movies.router, prefix="/users", tags=["users"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
