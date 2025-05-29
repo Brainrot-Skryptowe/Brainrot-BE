997b5538fb3e972240ef198ee9e389e313b1d7f7
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2025-05-29 15:45:48 +0200

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 
 from app.api.v1.api_v1 import api_router
 from app.db.session import engine
 
 app = FastAPI(title="Brainrot API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
